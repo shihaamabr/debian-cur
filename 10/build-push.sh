1ca23cf095044843c2e6ad7a43119ceccdd23fa0
Shihaam Abdul Rahman <shihaam@shihaam.me>
2023-10-08 23:15:45 +0500

    hedhevihaa arch eh supported now

--- 10/build-push.sh
+++ 10/build-push.sh
@@ -14,6 +14,10 @@ docker manifest create git.shihaam.dev/dockerfiles/debian-curl:10 \
 	git.shihaam.dev/dockerfiles/debian-curl:x86-10 \
 	git.shihaam.dev/dockerfiles/debian-curl:arm-10
 
+#docker manifest create git.shihaam.dev/dockerfiles/debian-curl/debian-curl:test \
+#git.shihaam.dev/dockerfiles/debian-curl/debian-curl:test-git.shihaam.dev/dockerfiles/debian-curl/debian-curl:test-linux-amd64-v3 \
+#git.shihaam.dev/dockerfiles/debian-curl/debian-curl:test-git.shihaam.dev/dockerfiles/debian-curl/debian-curl:test-linux-arm64
+
 #docker manifest annotate git.shihaam.dev/dockerfiles/debian-curl:10 \
 #git.shihaam.dev/dockerfiles/debian-curl:x86-10  --os linux --arch amd64
 
